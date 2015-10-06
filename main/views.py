c85304cf8cfea5c39a315966a2fd7506a11b30d4
gausszh <gauss.zh@gmail.com>
2015-10-06 10:41:59 +0800

    添加一个cacheback方法

--- main/views.py
+++ main/views.py
@@ -1,11 +1,34 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
+from cacheback.decorators import cacheback
 from main.tasks import document_length
 
+logger = logging.getLogger("")
 # Create your views here.
 
 def celery_task(request):
-    url = "http://baidu.com"
-    t = document_length.delay(request.GET.get("tt"))
-    print t, type(t)
-    return HttpResponse("tt")
+    # url = "https://www.baidu.com"
+    # t = document_length.delay(request.GET.get("tt"))
+    # print t, type(t)
+    t = "request views"
+    logger.debug("debug, %s, %s", t, logger)
+    logger.info("info, %s, %s", t, logger)
+
+    t = _request_html_url()
+    print t
+    return HttpResponse(t[30:])
+
+
+@cacheback(3)
+def _request_html_url():
+    url = "https://www.baidu.com"
+    import requests
+    r = requests.get(url)
+    import time
+    time.sleep(5)
+    t = r.content[:100]
+    logger.debug("debug, %s, %s", t, logger)
+    logger.info("info, %s, %s", t, logger)
+
+    return t
